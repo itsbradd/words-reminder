3bf9446a9869d8311fea4e83b470a67518ada79d
Brad <sonngoc.me@gmail.com>
2023-10-15 20:14:54 +0700

    [workspace] feat: Implement commit msg lint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,30 +1,43 @@
 import type {UserConfig} from '@commitlint/types';
-import {RuleConfigSeverity} from '@commitlint/types';
 
 const Configuration: UserConfig = {
-    parserOpts: {
-        headerPattern: /^(\w*)\((\w*)\)-(\w*)\s(.*)$/,
-        headerCorrespondence: ['type', 'scope', 'ticket', 'subject'],
-    },
     /*
-     * Resolve and load @commitlint/format from node_modules.
-     * Referenced package must be installed
-     */
-    formatter: '@commitlint/format',
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^\[(\w*)]\s(\w*)(\(.+\))?:\s(.*)$/,
+            headerCorrespondence: ['scope', 'type', 'ticket', 'subject'],
+        }
+    },
     /*
      * Any rules defined here will override rules from @commitlint/config-conventional
      */
     rules: {
-        'type-enum': [RuleConfigSeverity.Error, 'always', ['foo']],
+        'scope': async () => {
+            // TODO: Setup auto generate scopes based on NX Workspace
+            // const graph = await createProjectGraphAsync({
+            //     exitOnError: true
+            // })
+            // const projectMap = await createProjectFileMapUsingProjectGraph(graph)
+            return [2, 'always', ['workspace', 'api', 'web']]
+        },
+        'subject-case': [2, 'always', 'sentence-case'],
     },
-    /*
-     * Functions that return true if commitlint should ignore the given message.
-     */
-    ignores: [(commit) => commit === ''],
-    /*
-     * Whether commitlint uses the default ignore rules.
-     */
-    defaultIgnores: true,
+    plugins: [
+        {
+            rules: {
+                'scope': ({ scope }, _when, value: string[] = []) => {
+                    return [
+                        value.includes(scope || ''),
+                        `[scope] should contains invalid Nx project name in this workspace (${value})`
+                    ]
+                }
+            }
+        }
+    ],
 };
 
 module.exports = Configuration;
